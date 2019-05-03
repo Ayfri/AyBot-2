5d57e06158d406f814d66c7941fe2ef79d6a0cba
Bethoth <45798269+ThePizzaDu62@users.noreply.github.com>
2019-05-04 00:24:35 +0200

    Translate all string litterals in English

--- commands/administration/role.js
+++ commands/administration/role.js
@@ -2,7 +2,7 @@ const argsError = require("../../functions/argsError");
 module.exports.run = async (client, message, args, argsError) => {
 	let role = args[0];
 	let guildRoles = message.guild.roles.array();
-	if(role === "tous") {
+	if(role === "all") {
 		let roleOf = args[1];
 		role = args[2];
 		try {
@@ -12,26 +12,26 @@ module.exports.run = async (client, message, args, argsError) => {
 			for(let roletoFind2 of guildRoles) {
 				if(roletoFind2.name.includes(role)) role = roletoFind2;
 			}
-		} catch(e) { return message.channel.send(argsError("Le rôle n'a pas été trouvé.", "Erreur sur le premier argument.",client.commands.get(__filename.slice(__dirname.length + 1, __filename.length - 3)))); }
+		} catch(e) { return message.channel.send(argsError("The role wasn't found.", "Error on the first argument.",client.commands.get(__filename.slice(__dirname.length + 1, __filename.length - 3)))); }
 
 		let membersRole = message.guild.roles.get(roleOf.id).members;
 		membersRole.forEach(member => {
 			member.addRole(role.id);
 		});
-		return message.channel.send(`Le rôle \`${role.name}\` a été donné à tout les membres qui ont le rôle \`${roleOf.name}\`.`);
+		return message.channel.send(`The role \`${role.name}\` was given to all the members who have the role \`${roleOf.name}\`.`);
 	} else try {
 		for(let roletoFind of guildRoles) {
 			if(roletoFind.name.includes(role)) role = roletoFind;
 		}
-	} catch(e) { return message.channel.send(argsError("Le rôle n'a pas été trouvé.", "Erreur sur le deuxième argument.",client.commands.get(__filename.slice(__dirname.length + 1, __filename.length - 3)))); }
+	} catch(e) { return message.channel.send(argsError("The role wasn't found.", "Error on the second argument.",client.commands.get(__filename.slice(__dirname.length + 1, __filename.length - 3)))); }
 
 	if(message.guild.roles.has(role.id)) {
 		message.guild.members.forEach(member => {
 			member.addRole(role);
 		});
-		return message.channel.send(`Le rôle \`${role.name}\` a été donné à tout les membres.`);
+		return message.channel.send(`The role \`${role.name}\` was given to all members.`);
 	}
-	return message.channel.send(argsError("Le rôle n'a pas été trouvé.", "Erreur sur l'argument.",client.commands.get(__filename.slice(__dirname.length + 1, __filename.length - 3))));
+	return message.channel.send(argsError("The role wasn't found.", "Error on the argument.",client.commands.get(__filename.slice(__dirname.length + 1, __filename.length - 3))));
 }
 module.exports.config = {
 	category: "administration",
@@ -41,7 +41,7 @@ module.exports.config = {
 }
 
 module.exports.help = {
-	description: "Permet de donner un rôle à tout le monde ou de donner un rôle à tout les membres qui ont un rôle spécifique.",
-	utilisations: `role [nom de rôle à donner]\nrole tous [nom de rôle] [nom de rôle à donner]`,
-	exemples: `role Membre\nrole tous Membre Développeur`
+	description: "Give the possibility to give a role to everyone or to give a role to all members who have a specific role.",
+	utilisations: `role [name of the role to give]\nrole all [role name] [name of the role to give]`,
+	exemples: `role Member\nrole all Member Developer`
 }

ce36c64ea1a3f84e155e34ca33a7a4e8bfc767c1
Bethoth <45798269+ThePizzaDu62@users.noreply.github.com>
2019-05-04 00:41:15 +0200

    Update role.js

--- commands/administration/role.js
+++ commands/administration/role.js
@@ -36,7 +36,7 @@ module.exports.run = async (client, message, args, argsError) => {
 module.exports.config = {
 	category: "administration",
 	name: __filename.slice(__dirname.length + 1, __filename.length - 3),
-	englishName: name,
+	englishName: __filename.slice(__dirname.length + 1, __filename.length - 3),
 	aliases: [],
 	serverForced: true
 }
